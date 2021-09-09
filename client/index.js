8b48f700b070bcac74903d9f5d97fbd7bb8b9f9a
Kevin Gao <kevinzhugao@gmail.com>
2021-09-09 11:04:31 -0700

    Created basic outline for the overview widget

--- client/index.js
+++ client/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
 
+var overview = product => {
+  return product.description;
+}
+
 ReactDOM.render( <App/>, document.getElementById('app'));
