4f66a7e20849b0d1fc1cd65476275a7d3a38ff42
Kevin Gao <kevinzhugao@gmail.com>
2021-09-13 11:13:10 -0700

    Added route to server instead of inside component. Created a config.js to store Auth tokens

--- client/components/overview/ProductOverviewWidget.jsx
+++ client/components/overview/ProductOverviewWidget.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 class ProductOverviewWidget extends React.Component {
   constructor() {
@@ -12,16 +12,12 @@ class ProductOverviewWidget extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?sort=newest&product_id=40344&count=100', {
-      headers: { Authorization: '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' },
-    })
-      .then((response) => {
-        this.setState({
-          rating: response.data.results
-            .map((review) => review.rating)
-            .reduce((previous, current) => previous + current, 0) / response.data.results.length,
-        });
-      });
+    // axios.get('/product/rating/40355')
+    //   .then((response) => {
+    //     this.setState({
+    //       rating: response.data.toFixed(2),
+    //     });
+    //   });
   }
 
   render() {
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const { authToken } = require('../config');
 
 // Test Demo Router
 const router = require('./routes/test');
@@ -10,6 +12,18 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', router);
+
+app.get('/product/rating/:id', (req, res) => {
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?sort=newest&product_id=${req.params.id}&count=100`, {
+    headers: { Authorization: authToken },
+  })
+    .then((response) => {
+      res.send(JSON.stringify(response.data.results
+        .map((review) => review.rating)
+        .reduce((previous, current) => previous + current, 0) / response.data.results.length));
+    })
+    .catch((err) => res.send(err));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at localhost:${PORT}!`);
