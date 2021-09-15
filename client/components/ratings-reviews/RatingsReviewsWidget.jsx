2505e5aeee7acc60846a7e77910637697a46a550
Justin Beere <beere.justin@gmail.com>
2021-09-15 11:02:09 -0700

    set mock data as default props for my widget

--- client/components/ratings-reviews/RatingsReviewsWidget.jsx
+++ client/components/ratings-reviews/RatingsReviewsWidget.jsx
@@ -5,7 +5,10 @@ import PropTypes from 'prop-types';
 import './RatingsReviews.css';
 import RatingBreakdown from './RatingBreakdown/RatingBreakdown';
 
-const RatingsReviewsWidget = ({ reviews }) => (
+import mockReviewMeta from '../../mock-data/sample-review-meta.json';
+import mockReviews from '../../mock-data/sample-reviews.json';
+
+const RatingsReviewsWidget = ({ reviews, reviewMeta, productId }) => (
   <div className="ratings-reviews">
     <div className="rr-title"><h2>RATINGS & REVIEWS</h2></div>
     <div className="rr-widget-container">
@@ -15,10 +18,17 @@ const RatingsReviewsWidget = ({ reviews }) => (
   </div>
 );
 
+RatingsReviewsWidget.defaultProps = {
+  productId: mockReviews.product,
+  reviews: mockReviews.results,
+  reviewMeta: mockReviewMeta,
+};
+// TODO - time permitting figure out how to pass
+// prop-types the proper format for your props
 RatingsReviewsWidget.propTypes = {
-  reviews: PropTypes.arrayOf(PropTypes.string).isRequired,
-  // PropTypes.arrayOf(PropTypes.string)
-  // PropTypes.string.isRequired
+  productId: PropTypes.string,
+  reviews: PropTypes.array,
+  reviewMeta: PropTypes.object,
 };
 
 export default RatingsReviewsWidget;
