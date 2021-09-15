42846ac17289472d1d8374d7bd44eb59f4eb8821
Kevin Gao <kevinzhugao@gmail.com>
2021-09-15 05:45:57 -0700

    Created basic outline for product info component

---
+++ client/components/overview/Info.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Info = ({ product, styles }) => (
+  <div>
+    <p>Stars here <a href="#ratings-reviews">Reall all # reviews</a></p>
+    <p>Category here</p>
+    <h1>Product name here</h1>
+    <p>Price here</p>
+  </div>
+);
+
+export default Info;
