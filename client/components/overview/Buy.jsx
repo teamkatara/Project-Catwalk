e5b8dca2ed55f87c388947169da822647ccd3f87
Kevin Gao <kevinzhugao@gmail.com>
2021-09-17 14:33:59 -0700

    Size dropdown now exists and detects changes

--- client/components/overview/Buy.jsx
+++ client/components/overview/Buy.jsx
@@ -7,18 +7,36 @@ class Buy extends React.Component {
   constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      size: null,
+      quantity: null,
+      quantityOptions: null,
+    };
+
+    this.sizeSelect = this.sizeSelect.bind(this);
+  }
+
+  sizeSelect(event) {
+    console.log(event.target.value);
   }
 
   render() {
     return (
       <div>
-        <Size />
-        <Quantity />
-        <AddtoCart />
+        <Size selectedStyle={this.props.selectedStyle} sizeSelect={this.sizeSelect} />
+        <Quantity selectedStyle={this.props.selectedStyle} quantityOptions={this.state.quantityOptions} />
+        <AddtoCart size={this.state.size} quantity={this.state.quantity} />
       </div>
     );
   }
 }
+
+// const Buy = ({ selectedStyle }) => (
+//   <div>
+//     <Size selectedStyle={selectedStyle} />
+//     <Quantity selectedStyle={selectedStyle} />
+//     <AddtoCart />
+//   </div>
+// );
 
 export default Buy;
--- client/components/overview/Size.jsx
+++ client/components/overview/Size.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Size = () => (
-  <div>Hello from Size</div>
+const Size = ({ selectedStyle, sizeSelect }) => (
+  <select onChange={sizeSelect}>
+    <option>Select Size</option>
+    {Object.values(selectedStyle.skus).map((sku) => {
+      if (sku.quantity > 0) {
+        return <option>{sku.size}</option>;
+      }
+    })}
+  </select>
 );
 
 export default Size;
