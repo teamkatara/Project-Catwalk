8be131fde61e35e23d01a9cdeb3cbb860d1b6a2e
Ryan Rhoads <rhoads.ryan.grant@gmail.com>
2021-09-21 15:37:09 -0700

    Implemented Context inside QA Components to utilize useEffect

--- client/components/qa/QuestionsAndAnswersWidget.jsx
+++ client/components/qa/QuestionsAndAnswersWidget.jsx
@@ -1,17 +1,31 @@
 // eslint-disable-next-line no-use-before-define
 import './qa-styles.css';
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import axios from 'axios';
 import QuestionList from './QuestionList';
 import QuestionData from '../../mock-data/sample-questions.json';
+import ProductContext from '../ProductContext';
 
 const QuestionsAndAnswersWidget = () => {
-  const questions = QuestionData.results;
-  const allQuestions = questions;
+  const mockQuestions = QuestionData.results;
+  // const allQuestions = mockQuestions;
+
+  const productId = useContext(ProductContext);
+  const [questions, setQuestions] = React.useState(mockQuestions);
+
+  useEffect(() => {
+    axios.get(`/qa/questions/${productId}`)
+      .then((response) => {
+        console.log(response);
+        setQuestions(response);
+      })
+      .catch();
+  }, [productId]);
 
   return (
     <div className="questions-answers">
       <div id="qa-title">QUESTIONS & ANSWERS</div>
-      <QuestionList questions={allQuestions} />
+      <QuestionList questions={questions} />
     </div>
   );
 };
