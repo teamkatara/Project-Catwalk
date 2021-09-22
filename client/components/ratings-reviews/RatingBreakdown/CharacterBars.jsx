3c19eefbef2d509e402c43d150b544f59c7a348c
Justin Beere <beere.justin@gmail.com>
2021-09-22 15:55:38 -0700

    refactored if statements inside ProductBreakdown to constant time

--- client/components/ratings-reviews/RatingBreakdown/CharacterBars.jsx
+++ client/components/ratings-reviews/RatingBreakdown/CharacterBars.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 // The characteristics include Size, Width, Comfort, Quality, Length, and Fit.
 const CharacterBars = ({ characters }) => {
-  const availableCharacters = Object.keys(characters);
   const [showSize, setShowSize] = useState(false);
   const [sizeValue, setSizeValue] = useState(0);
   const [showWidth, setShowWidth] = useState(false);
@@ -23,44 +22,44 @@ const CharacterBars = ({ characters }) => {
   };
 
   useEffect(() => {
-    if (availableCharacters.indexOf('Size') !== -1) {
+    if (characters.Size) {
       const size = convertValue(characters.Size.value);
       setSizeValue(size);
       setShowSize(true);
     }
-    if (availableCharacters.indexOf('Width') !== -1) {
+    if (characters.Width) {
       const width = convertValue(characters.Width.value);
       setWidthValue(width);
       setShowWidth(true);
     }
-    if (availableCharacters.indexOf('Comfort') !== -1) {
+    if (characters.Comfort) {
       const comfort = convertValue(characters.Comfort.value);
       setComfortValue(comfort);
       setShowComfort(true);
     }
-    if (availableCharacters.indexOf('Quality') !== -1) {
+    if (characters.Quality) {
       const quality = convertValue(characters.Quality.value);
       setQualityValue(quality);
       setShowQuality(true);
     }
-    if (availableCharacters.indexOf('Length') !== -1) {
+    if (characters.Length) {
       const length = convertValue(characters.Length.value);
       setLengthValue(length);
       setShowLength(true);
     }
-    if (availableCharacters.indexOf('Fit') !== -1) {
+    if (characters.Fit) {
       const fit = convertValue(characters.Fit.value);
       setFitValue(fit);
       setShowFit(true);
     }
-  }, [availableCharacters]);
+  }, [characters]);
   // The characteristics include Size, Width, Comfort, Quality, Length, and Fit.
   return (
     <div>
       {showSize &&
         (
         <div className="product-size">
-          <span style={{ fontWeight: 'bold' }}>Size</span>
+          <span style={{ fontWeight: 'normal' }}>Size</span>
           <div className="character-bar">
             <div className="character-pointer">
               <i className="fas fa-caret-down fa-2x" aria-hidden="true" style={{ paddingLeft: `${sizeValue}%` }} />
@@ -72,7 +71,7 @@ const CharacterBars = ({ characters }) => {
       {showWidth &&
         (
         <div className="product-width">
-          <span style={{ fontWeight: 'bold' }}>Width</span>
+          <span style={{ fontWeight: 'normal' }}>Width</span>
           <div className="character-bar">
             <div className="character-pointer">
               <i className="fas fa-caret-down fa-2x" aria-hidden="true" style={{ paddingLeft: `${widthValue}%` }} />
@@ -84,7 +83,7 @@ const CharacterBars = ({ characters }) => {
       {showComfort &&
         (
         <div className="product-comfort">
-          <span style={{ fontWeight: 'bold' }}>Comfort</span>
+          <span style={{ fontWeight: 'normal' }}>Comfort</span>
           <div className="character-bar">
             <div className="character-pointer">
               <i className="fas fa-caret-down fa-2x" aria-hidden="true" style={{ paddingLeft: `${comfortValue}%` }} />
@@ -96,7 +95,7 @@ const CharacterBars = ({ characters }) => {
       {showQuality &&
         (
         <div className="product-quality">
-          <span style={{ fontWeight: 'bold'}}>Quality</span>
+          <span style={{ fontWeight: 'normal'}}>Quality</span>
           <div className="character-bar">
             <div className="character-pointer">
               <i className="fas fa-caret-down fa-2x" aria-hidden="true" style={{ paddingLeft: `${qualityValue}%` }} />
@@ -108,7 +107,7 @@ const CharacterBars = ({ characters }) => {
       {showLength &&
         (
         <div className="product-length">
-          <span style={{ fontWeight: 'bold' }}>Length</span>
+          <span style={{ fontWeight: 'normal ' }}>Length</span>
           <div className="character-bar">
             <div className="character-pointer">
               <i className="fas fa-caret-down fa-2x" aria-hidden="true" style={{ paddingLeft: `${lengthValue}%` }} />
@@ -120,7 +119,7 @@ const CharacterBars = ({ characters }) => {
       {showFit &&
         (
         <div className="product-fit">
-          <span style={{ fontWeight: 'bold' }}>Fit</span>
+          <span style={{ fontWeight: 'normal' }}>Fit</span>
           <div className="character-bar">
             <div className="character-pointer">
               <i className="fas fa-caret-down fa-2x" aria-hidden="true" style={{ paddingLeft: `${fitValue}%` }} />
