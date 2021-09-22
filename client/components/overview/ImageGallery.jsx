070d5ab59e5b04f5712188ac25365575856a7340
Kevin Gao <kevinzhugao@gmail.com>
2021-09-22 15:59:47 -0700

    Image gallery CSS

--- client/components/overview/ImageGallery.jsx
+++ client/components/overview/ImageGallery.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const ImageGallery = ({ photos, current, setCurrent }) => (
+  // <div className="image-gallery">
+  //   <div id="image-gallery">
+  //     {photos.map((photo, i) => i === current
+  //       ? <div onClick={() => setCurrent(i)} className="photo-container"><img className="photo-entry selected" src={photo.thumbnail_url} /></div>
+  //       : <div onClick={() => setCurrent(i)} className="photo-container"><img className="photo-entry" src={photo.thumbnail_url} /></div>)}
+  //   </div>
+  // </div>
+
   <div className="image-gallery">
     <div id="image-gallery">
       {photos.map((photo, i) => i === current
-        ? <div onClick={() => setCurrent(i)} className="photo-container selected"><img className="photo-entry" src={photo.thumbnail_url} /></div>
-        : <div onClick={() => setCurrent(i)} className="photo-container"><img className="photo-entry" src={photo.thumbnail_url} /></div>)}
+        ? <img onClick={() => setCurrent(i)} className="photo-entry selected" src={photo.thumbnail_url} />
+        : <img onClick={() => setCurrent(i)} className="photo-entry" src={photo.thumbnail_url} />)}
     </div>
   </div>
 );
