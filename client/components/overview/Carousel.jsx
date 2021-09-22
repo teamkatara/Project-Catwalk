1143e65330848787ce475340607761942583344b
Kevin Gao <kevinzhugao@gmail.com>
2021-09-21 18:26:14 -0700

    Completed expanded view modal

--- client/components/overview/Carousel.jsx
+++ client/components/overview/Carousel.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
 import { RiFullscreenFill } from 'react-icons/ri';
 import ImageGallery from './ImageGallery';
+import Modal from './Modal';
 
 const Carousel = ({ clickedStyle }) => {
   const [current, setCurrent] = useState(0);
   const [showModal, setModal] = useState(false);
   const length = clickedStyle.photos.length;
+  const url = useRef();
 
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1);
@@ -19,14 +21,18 @@ const Carousel = ({ clickedStyle }) => {
   return (
     <div className="slider">
       <ImageGallery photos={clickedStyle.photos} current={current} setCurrent={setCurrent} />
-      <RiFullscreenFill className="expand" />
+      <Modal url={url.current} showModal={showModal} setModal={setModal} />
+      <RiFullscreenFill className="expand" onClick={() => setModal(!showModal)} />
       <BiLeftArrowAlt className="left-arrow" onClick={prevSlide} />
       <BiRightArrowAlt className="right-arrow" onClick={nextSlide} />
-      {clickedStyle.photos.map((photo, i) => (
-        <div className={i === current ? 'slide active' : 'slide'} key={i}>
-          {i === current && <img className="images" src={photo.url} alt="product" />}
-        </div>
-      ))}
+      {clickedStyle.photos.map((photo, i) => {
+        if (i === current) { url.current = photo.url; }
+        return (
+          <div className={i === current ? 'slide active' : 'slide'} key={i}>
+            {i === current && <img className="images" src={photo.url} alt="product" />}
+          </div>
+        );
+      })}
     </div>
   );
 };
